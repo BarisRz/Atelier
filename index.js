3d0f9d3f7e364225e6b64b2ea39b728e53427234
baris.g <67648495+BarisRz@users.noreply.github.com>
2023-10-03 10:25:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import chalk from "chalk";
 console.log(chalk.blue("Morgane"));
 console.log(chalk.red("Mehdi"));
-console.log(chalk.green("Digo"));
+console.log(chalk.green("Diogo"));
 console.log(chalk.yellow("Baris"));
